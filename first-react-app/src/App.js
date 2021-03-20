5bfcc25c182f3d97e76b0fcba79824c51efdc922
Tyler Shamsuddoha <tylershamsuddoha@gmail.com>
2021-03-20 01:43:37 -0400

    Attempt at fixing project setup
    Started on button for adding a new TODO item
    Edited some comments in App.js

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -8,30 +8,37 @@ function App() {
   const [todoItems, setTodoItems] = useState(null)
 
   useEffect(() => {
+    // This is the onLoad() section
     // do something when app loads up
     console.log("Loaded up");
 
+    // If there are no todoItems, fetch items
     if (!todoItems) {
       fetch("http://localhost:8080/api/todoItems")
         .then((response) => response.json())
         .then((data) => {
           console.log("To do items list: ", data);
+          // Update the 
           setTodoItems(data);
         });
     }
   });
-
-
-
-
+  
   return (
+  <>
+  <div>
+    <button> Add new To Do Item </button>
+  </div>
+
     <div>
       {todoItems
         ? todoItems.map(todoItem => {
           return <TodoItem key={todoItem.id} data={todoItem} />;
-        }) : 'loading data'}
+        }) : 'Need to fetch items...'}
     </div>
+  </>
   );
+
 }
 
 export default App;
